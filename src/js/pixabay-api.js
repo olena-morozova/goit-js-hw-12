220f1765b86cbce438c4339df44b8aabeb6df50a
Olena Morozova <appsapps530@gmail.com>
2025-03-23 00:01:44 +0200

    add async/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,22 +4,22 @@ import axios from "axios";
 const API_KEY = "49372428-d1d84138fa434fefe0cc10176";  
 const BASE_URL = "https://pixabay.com/api/";  
 
-export function fetchImages(query) {  
+export async function fetchImages(query, page = 1, limit = 15) {  
     const params = {  
         key: API_KEY,  
         q: query,  
         image_type: "photo",  
         orientation: "horizontal",  
         safesearch: true,  
+        page: page,
+        per_page: limit
     };  
-
-    return axios
-        .get(BASE_URL, { params })  
-        .then(response => {
-            return response.data;
-        }) 
-        .catch(error => {  
+    try {
+        const response = await axios.get(BASE_URL, { params });
+        return response.data;
+} catch(error) {  
             console.error("Помилка запиту:", error);  
-            return []; 
-        });  
+            throw error; 
+        };  
+    
 }
--- src/main.js
+++ src/main.js
@@ -7,36 +7,71 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(".form");  
 const loader = document.querySelector(".loader");
+const loadMoreBtn = document.querySelector(".load-more");
 
-form.addEventListener("submit", (event) => {  
-    event.preventDefault();  
+let query = "";
+let page = 1;
+const limit = 15;
+let totalHits = 0;
 
-    const query = event.target.elements["search-text"].value.trim(); 
+form.addEventListener("submit", onSearchSubmit);
+loadMoreBtn.addEventListener("click", onLoadMoreClick);
 
-    if (query === "") {  
-        console.log("Поле пошуку порожнє");  
-        return;  
-    }  
-
+async function onSearchSubmit(event) {
+    event.preventDefault();
+    query = event.target.elements["search-text"].value.trim();
+    
+    if (query === "") return;
+    
+    page = 1;
     clearGallery();
-loader.classList.remove("hidden");
+    loader.classList.remove("hidden");
+    loadMoreBtn.classList.add("hidden");
 
-    fetchImages(query)  
-        .then(data => {  
-            if (data.hits.length === 0) {  
-                iziToast.error({
-    message: 'Sorry, there are no images matching your search query. Please try again!',
-    position: 'topRight'
-});
-                return;  
-            }  
-            renderImages(data.hits);  
-        })  
-        .catch(error => console.log("Помилка отримання даних:", error))
-        .finally(() => {
+      try {
+          const data = await fetchImages(query, page, limit);
+          totalHits = data.totalHits;
+        if (data.hits.length === 0) {
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight'
+            });
+            return;
+        }
+        renderImages(data.hits);
+         if (limit * page < totalHits) {
+      loadMoreBtn.classList.remove("hidden");
+    }
+    } catch (error) {
+        console.log("Помилка отримання даних:", error)
+    } finally {
             
         loader.classList.add("hidden");
-      form.reset();
-      
+      form.reset();      
+    }
+}
+
+async function onLoadMoreClick() {
+    page += 1;
+    loadMoreBtn.classList.add("hidden");
+    loader.classList.remove("hidden");
+    try {          
+        const data = await fetchImages(query, page, limit);
+        renderImages(data.hits);
+        if (limit * page < totalHits) {
+            loadMoreBtn.classList.remove("hidden");
+        } else {
+            //return
+            iziToast.error({
+      position: "topRight",
+      message: "We're sorry, but you've reached the end of search results."
     });
-});
+  }        
+    } catch (error) {
+       console.log(error); 
+    } finally {
+       loader.classList.add("hidden"); 
+    }
+
+}
+
